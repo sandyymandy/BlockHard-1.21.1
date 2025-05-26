31eb3f8b271271e9ec16dba5d5da76afe7c3497e
SandyMandy <Sandaru.jayas@outlook.com>
2025-05-26 15:24:30 +0930

    Initial commit

---
+++ src/main/java/com/sandymandy/blockhard/BlockHardClient.java
@@ -0,0 +1,10 @@
+package com.sandymandy.blockhard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockHardClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/sandymandy/blockhard/BlockHardDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sandymandy.blockhard;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlockHardDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
